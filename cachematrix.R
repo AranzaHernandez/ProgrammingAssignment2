6db37a963dfb4c138b5f6c409d98e5dbd36a11b8
Aranza <ara171090@hotmail.com>
2015-03-21 22:55:49 -0600

    cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first part sets the matrix in the cache, and defines a list of functions to set 
+## and get the original and inverse matrix.
+##The next part cheks if the inverse was calculated and gives it back, otherwise
+##calculates it.
 
-## Write a short comment describing this function
+## This defines a function that has a matrix for argument, and returns a list with 
+##functions that will be used in the next part.
 
 makeCacheMatrix <- function(x = matrix()) {
+## Sets the inverse matrix as empty         
+        inv <- NULL
+## Defines the set function. It saves the matrix and the inverse in the cache       
+        set <- function(y){
+                x <<- y
+                inv <<- NULL 
+        }
+## Defines the get function to call the original matrix
+        get <- function() x
+## Defines the setinv function to save the inverse matrix to the cache
+        setinv <- function(inv) inv <<- solve
+## Defines the getinv function to call the inverse matrix from the cache
+        getinv <- function() inv
+## Makes a list with this functions
+        list(set = set, get = get, setinv = setinv, getinv = getinv )
 
 }
 
 
-## Write a short comment describing this function
+## This function takes a list that should have the functions for get the original and
+## inverse matix, and the function for set the inverse matrix. 
+##If the inverse has been calculated, prints the result, otherwise it calculates it.
 
 cacheSolve <- function(x, ...) {
+## Gets the inverse matrix from the cache        
+        inv <- x$getinv()
+## If the inverse matrix has already been calculated, it just prints the result        
+        if(!is.null(inv) ){
+                inv
+        }
+## If the inverse han not been calculated, gets the original matrix and calls it "mat"
+        mat <- x$get()
+## and gets the invese matrix by the function solve       
+        inv <- solve(mat,...)
+        x$setinv(inv)
+        inv
         ## Return a matrix that is the inverse of 'x'
 }
